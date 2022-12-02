74c465d25d24fdab9158b8ed70832e05ed64baa6
Mateo <paezball@ualberta.ca>
2022-12-02 01:06:56 -0700

    tests pass, should be done

--- sockets.py
+++ sockets.py
@@ -13,8 +13,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+#!/usr/bin/env python
+# coding: utf-8
+# Copyright (c) 2022 Mateo Paez
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 import flask
-from flask import Flask, request
+from flask import Flask, request, redirect
 from flask_sockets import Sockets
 import gevent
 from gevent import queue
@@ -25,6 +40,27 @@ import os
 app = Flask(__name__)
 sockets = Sockets(app)
 app.debug = True
+
+# Took all of the client stuff from class example
+
+clients = list()
+
+def send_all(msg):
+    for client in clients:
+        client.put(msg)
+
+def send_all_json(obj):
+    send_all(json.dumps(obj))
+
+class Client:
+    def __init__(self):
+        self.queue = queue.Queue()
+
+    def put(self, v):
+        self.queue.put_nowait(v)
+    
+    def get(self):
+        return self.queue.get()
 
 class World:
     def __init__(self):
@@ -63,25 +99,56 @@ myWorld = World()
 
 def set_listener( entity, data ):
     ''' do something with the update ! '''
+    entity_dict = {}
+    entity_dict[entity] = data
+    send_all_json(entity_dict)
+
 
 myWorld.add_set_listener( set_listener )
         
 @app.route('/')
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return redirect("/static/index.html", 302)
 
 def read_ws(ws,client):
     '''A greenlet function that reads from the websocket and updates the world'''
     # XXX: TODO IMPLEMENT ME
-    return None
+    # Taken from class examples
+    try:
+        while True:
+            msg = ws.receive()
+            print("WEBSOCKET RECV: %s" % msg)
+            if msg is not None:
+                packet = json.loads(msg)
+                send_all_json(packet)
+                for key, value in packet.items():
+                    myWorld.set(key, value)
+            else:
+                break
+
+    except:
+        '''Done'''
 
 @sockets.route('/subscribe')
 def subscribe_socket(ws):
     '''Fufill the websocket URL of /subscribe, every update notify the
        websocket and read updates from the websocket '''
     # XXX: TODO IMPLEMENT ME
-    return None
+    # Taken from class examples
+    client = Client()
+    clients.append(client)
+    g = gevent.spawn(read_ws, ws, client)
+    try:
+        while True:
+            # block here
+            msg = client.get()
+            ws.send(msg)
+    except Exception as e:
+        print("Websocket Error %s" % e)
+    finally:
+        clients.remove(client)
+        gevent.kill(g)
 
 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
@@ -99,23 +166,27 @@ def flask_post_json():
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    data = flask_post_json()
+    for key in data:
+        myWorld.update(entity, key, data[key])
+    return json.dumps(myWorld.get(entity))
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    return json.dumps(myWorld.world())
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    return json.dumps(myWorld.get(entity))
 
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return json.dumps(myWorld.world())
 
 
 
